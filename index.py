81d402180d5785ff0c79ec0c96b4bbe8c24c20df
alexHooper <alexmohack@gmail.com>
2018-09-24 12:41:22 +0530

    correct some errors

--- index.py
+++ index.py
@@ -24,13 +24,13 @@ def index():
 	return render_template('index.html')
 
 
-@app.route('/get-movie-details', methods=['POST'])
+@app.route('/get-movie-detail', methods=['POST'])
 def get_movie_detail():
 	data = request.get_json(silent=True)
-	movie = data['queryResult']['parameters']['moive']
+	movie = data['queryResult']['parameters']['movie']
 	api_key = os.getenv('OMDB_API_KEY')
 
-	movie_detail = requests.get('http://www.omdbapi.com/?t={0}&apikey={1}'.format(movie, api_key))
+	movie_detail = requests.get('http://www.omdbapi.com/?t={0}&apikey={1}'.format(movie, api_key)).content
 	movie_detail = json.loads(movie_detail)
 	response = """
 		Title : {0}
@@ -66,10 +66,10 @@ def send_message():
 	project_id = os.getenv('DIALOGFLOW_PROJECT_ID')
 	fulfillment_text = detect_intent_texts(project_id, "unique", message, 'en')
 	response_text = {"message": fulfillment_text}
-	socketId = request.form['socketId']
-	pusher_client.trigger('movie_bot', 'new_message',
-		{'human_message': message, 'bot_message': fulfillment_text},
-		socketId)
+	# socketId = request.form['socketId']
+	# pusher_client.trigger('movie_bot', 'new_message',
+	# 	{'human_message': message, 'bot_message': fulfillment_text},
+	# 	socketId)
 
 	return jsonify(response_text)
 
